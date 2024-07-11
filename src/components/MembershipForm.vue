49941fdf07197e7e243f35900a9d7958c512247d
Andrew K <andrew.keym@gmail.com>
2024-07-11 10:55:19 -0400

    ios specific select styles

--- src/components/MembershipForm.vue
+++ src/components/MembershipForm.vue
@@ -324,8 +324,17 @@ input, select {
   background-color: #f8F9fa;
 }
 
-select {
-  min-height: 52px;
+/* iOS-specific adjustments */
+@supports (-webkit-touch-callout: none) {
+  select {
+    -webkit-appearance: none;
+    appearance: none;
+    background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+    background-repeat: no-repeat;
+    background-position: right 0.7em top 50%;
+    background-size: 0.65em auto;
+    padding-right: 1.5em;
+  }
 }
 
 fieldset input {
